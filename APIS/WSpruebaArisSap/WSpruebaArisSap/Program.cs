c8ecf4056113836a5489030dc1e6ae5ad83bb6e3
Gean Segura Sánchez <120821919+GeanSegura@users.noreply.github.com>
2025-05-14 18:39:59 -0500

    14/05/2025 Gean

--- APIS/WSpruebaArisSap/WSpruebaArisSap/Program.cs
+++ APIS/WSpruebaArisSap/WSpruebaArisSap/Program.cs
@@ -1,9 +1,39 @@
+using Application.Interfaces;
+using Application.Services;
+using Dominio.Entidad;
+using Infrastructure.Configuration;
+using Infrastructure.Sap;
+using Infrastructure.Services;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+<<<<<<< Updated upstream
 // Add services to the container.
+=======
+
+builder.Services.Configure<SapSettings>(
+    builder.Configuration.GetSection("SapSettings"));
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.File(Path.Combine(AppContext.BaseDirectory, "log.txt"), rollingInterval: RollingInterval.Day)  // Guardar los logs en el archivo log.txt
+    .CreateLogger();
+
+//builder.Services.AddScoped<ILogger, _Logger>();
+builder.Services.AddSingleton<ILibraryInitializer, LibraryInitialize>();
+builder.Services.AddSingleton<IInitializerContextSAP, InitializerContextSAP>();
+builder.Services.AddSingleton<ILoggingService, LoggerServicio>();
+//builder.Services.AddSingleton<IEntregaService, ReplicaOdooEntregaSap>();
+//builder.Services.AddSingleton<EntregaService>();
+builder.Services.AddTransient<SapResultadoPedido>();
+builder.Services.AddTransient<SapResultadoEntrega>();
+builder.Services.AddTransient<SapResultadoFactura>();
+
+>>>>>>> Stashed changes
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+<<<<<<< Updated upstream
 var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
@@ -11,3 +41,18 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
+=======
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+>>>>>>> Stashed changes
--- APIS/WSpruebaArisSap/WSpruebaArisSap/obj/Debug/net8.0/WSpruebaArisSap.AssemblyInfo.cs
+++ APIS/WSpruebaArisSap/WSpruebaArisSap/obj/Debug/net8.0/WSpruebaArisSap.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WSpruebaArisSap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57587821b482a2c36de8bce32a53663485e867fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("WSpruebaArisSap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WSpruebaArisSap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
